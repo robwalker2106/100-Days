76e3f53c980e05d0eedcb766c05452a63d6b47e7
robwalker2106 <blkdragon2106@gmail.com>
2021-01-22 17:16:50 -0500

    Update main.py

--- Day-16-Start/main.py
+++ Day-16-Start/main.py
@@ -4,6 +4,11 @@ don = Turtle()
 my_screen = Screen()
 don.shape('turtle')
 don.color('purple4')
+don.pencolor('red1')
+don.forward(210)
+don.right(90)
+don.pencolor('green3')
+don.forward(60)
 
 my_screen.exitonclick()
 
