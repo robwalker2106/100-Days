8d09dd83516e5e7fac4f68e7f7b03faa131fe554
robwalker2106 <blkdragon2106@gmail.com>
2021-01-22 21:49:39 -0500

    Update main.py
    Changed the while loop to only use the input from the user. Deleted the static creation of the MenuItems.

--- Day-16-Coffee-Machine/main.py
+++ Day-16-Coffee-Machine/main.py
@@ -1,14 +1,9 @@
-from menu import Menu, MenuItem
+from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
 # Creates the three items on the menu using the MenuItem class.
 menu = Menu()
-
-# Creates
-espresso = MenuItem('espresso', 50, 0, 18, 1.5)
-latte = MenuItem('latte', 200, 150, 24, 2.5)
-cappuccino = MenuItem('cappuccino', 250, 100, 24, 3.0)
 
 # Creates the Resource object.
 coffee_machine = CoffeeMaker()
@@ -24,13 +19,6 @@ while is_on:
     drink = {}
     choice = input("What would you like? (espresso/latte/cappuccino/): ")
     # off is a hidden choice.
-    if choice == 'latte':
-        drink = latte
-    if choice == 'espresso':
-        drink = espresso
-    if choice == 'cappuccino':
-        drink = cappuccino
-
     if choice == 'off':
         is_on = False
         print("\nTurning the Coffee Machine off.")
@@ -39,9 +27,10 @@ while is_on:
         print('\nReport: ')
         coffee_machine.report()
         money.report()
-    elif menu.find_drink(choice):
+    elif choice not in menu.get_items():
+        print("I didn't understand your choice.")
+    else:
+        drink = menu.find_drink(choice)
         if coffee_machine.is_resource_sufficient(drink):
             if money.make_payment(drink.cost):
                 coffee_machine.make_coffee(drink)
-    else:
-        print("\nI did not understand your choice.")
