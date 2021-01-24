e414cede45da57273a76c7edc55bc13d713d64e7
robwalker2106 <blkdragon2106@gmail.com>
2021-01-24 16:55:13 -0500

    Update main.py

--- day-18-start/main.py
+++ day-18-start/main.py
@@ -1,27 +1,60 @@
 from turtle import Turtle, Screen
-from random import randint
+from random import randint, choice
 
 don = Turtle()
 
 don.shape('turtle')
 don.color('purple3')
-don.width(10)
-don.speed(10)
+#don.width(10)
+don.speed('fastest')
 screen = Screen()
 screen.colormode(255)
 
 
-def short_walk():
-    don.right(randint(1, 4) * 90)
-    r = randint(1, 255)
-    g = randint(1, 255)
-    b = randint(1, 255)
-    don.pencolor((r, g, b))
-    don.forward(20)
+def random_color():
+    """
+    Returns a random R, G, B color.
+    :return: Three integers.
+    """
+    r = randint(0, 255)
+    g = randint(0, 255)
+    b = randint(0, 255)
+
+    return r, g, b
 
 
-for _ in range(100):
-    short_walk()
+def spirograph(circles):
+    """
+    Takes an integer and creates a spirograph with the inputted amount of circles, each
+    with random line colors.
+    :param circles: int
+    :return: None
+    """
+    angles = 360 / circles
+    for _ in range(circles):
+        don.setheading(don.heading() + angles)
+        don.pencolor(random_color())
+        don.circle(100)
+
+
+spirograph(120)
+
+
+# def short_walk():
+#     """
+#     Creates a short walk with a random color and direction. There are three choices for the direction.
+#     (Left, Straight, or Right).
+#     :return: None
+#     """
+#     not_backwards = [-90, 0, 90]
+#     current = int(don.heading())
+#     don.setheading(current + choice(not_backwards))
+#     don.pencolor(random_color())
+#     don.forward(15)
+#
+#
+# for _ in range(200):
+#     short_walk()
 
 
 # def draw_shape(sides):
