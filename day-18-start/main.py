0a90cc0209460893eebd50aab86388b24178c630
robwalker2106 <blkdragon2106@gmail.com>
2021-01-24 16:19:46 -0500

    Update main.py

--- day-18-start/main.py
+++ day-18-start/main.py
@@ -5,33 +5,43 @@ don = Turtle()
 
 don.shape('turtle')
 don.color('purple3')
-
-
-sides = 3
+don.width(10)
+don.speed(10)
 screen = Screen()
 screen.colormode(255)
 
-for _ in range(3, 10):
+
+def short_walk():
+    don.right(randint(1, 4) * 90)
     r = randint(1, 255)
-    b = randint(1, 255)
     g = randint(1, 255)
-    for _ in range(sides):
-        don.pencolor((r, g, b))
-        don.forward(100)
-        don.right(360/sides)
-    sides += 1
+    b = randint(1, 255)
+    don.pencolor((r, g, b))
+    don.forward(20)
 
 
-# for _ in range(4):
-#     don.forward(40)
-#     don.right(90)
+for _ in range(100):
+    short_walk()
+
+
+# def draw_shape(sides):
+#     """
+#     Draws a shape with equal N sides.
+#     :param sides: int
+#     :return: None
+#     """
+#     r = randint(1, 255)
+#     b = randint(1, 255)
+#     g = randint(1, 255)
 #
+#     for _ in range(sides):
+#         don.pencolor((r, g, b))
+#         don.forward(100)
+#         don.right(360 / sides)
 
-# for _ in range(50):
-#     don.forward(5)
-#     don.penup()
-#     don.forward(5)
-#     don.pendown()
+
+# for i in range(3, 11):
+#     draw_shape(i)
 
 screen.screensize(600, 600)
 screen.exitonclick()
