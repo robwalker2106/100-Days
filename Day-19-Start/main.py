8a6a39019ad92fbb8a94a05635b85bfce05a8607
robwalker2106 <blkdragon2106@gmail.com>
2021-01-25 18:33:02 -0500

    Update main.py

--- Day-19-Start/main.py
+++ Day-19-Start/main.py
@@ -9,7 +9,27 @@ def move_forward():
     don.forward(10)
 
 
+def move_backward():
+    don.backward(10)
+
+
+def clockwise():
+    don.setheading(don.heading() + 10)
+
+
+def counterclockwise():
+    don.setheading(don.heading() - 10)
+
+
+def clear():
+    don.clear()
+
+
 screen.listen()
-screen.onkey(key="space", fun=move_forward)
+screen.onkey(key="w", fun=move_forward)
+screen.onkey(key="s", fun=move_backward)
+screen.onkey(key="a", fun=counterclockwise)
+screen.onkey(key="d", fun=clockwise)
+screen.onkey(key='c', fun=clear)
 
 screen.exitonclick()
