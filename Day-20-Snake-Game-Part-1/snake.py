313724990e8aa582425dbd53708517b57072b717
robwalker2106 <blkdragon2106@gmail.com>
2021-01-27 10:41:09 -0500

    Update
    Removed the MOVE_DISTANCE from being a constant and made it a parameter for creating the Snake object. It has a default of 20.

--- Day-20-Snake-Game-Part-1/snake.py
+++ Day-20-Snake-Game-Part-1/snake.py
@@ -1,6 +1,5 @@
 from turtle import Turtle
 
-MOVE_DISTANCE = 20
 UP = 90
 DOWN = 270
 LEFT = 180
@@ -8,8 +7,9 @@ RIGHT = 0
 
 
 class Snake:
-    def __init__(self):
+    def __init__(self, move_distance=20):
         self.snake_body = []
+        self.move_distance = move_distance
         self.starting_position()
         self.head = self.snake_body[0]
         self.tail = self.snake_body[-1]
@@ -31,12 +31,12 @@ class Snake:
         elif len(self.snake_body) == 1:
             turtle.setheading(self.snake_body[0].heading())
             turtle.goto(self.snake_body[0].position())
-            turtle.backward(MOVE_DISTANCE)
+            turtle.backward(self.move_distance)
             return turtle
         else:
             turtle.setheading(self.snake_body[-1].heading())
             turtle.goto(self.snake_body[-1].position())
-            turtle.backward(MOVE_DISTANCE)
+            turtle.backward(20)
             return turtle
 
     def move(self):
@@ -45,7 +45,7 @@ class Snake:
         for i in range(len(self.snake_body)):
             self.snake_body[-i].goto(x, y)
 
-        self.head.forward(MOVE_DISTANCE)
+        self.head.forward(self.move_distance)
 
     def turn_up(self):
         if self.head.heading() != DOWN:
