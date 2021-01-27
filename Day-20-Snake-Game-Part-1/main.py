802187f7f3e819616a4e00abd9d9ac275dd7278b
robwalker2106 <blkdragon2106@gmail.com>
2021-01-26 19:35:14 -0500

    Update main.py

--- Day-20-Snake-Game-Part-1/main.py
+++ Day-20-Snake-Game-Part-1/main.py
@@ -5,10 +5,40 @@ screen.setup(width=600, height=600)
 screen.bgcolor('black')
 screen.title('Snake Game')
 
+# Starting coordinates for the snake.
+x, y = 0, 0
+snake_body = []
 
 
+def create_segment(x, y):
+    """
+    Receives an x and y coordinate and creates a segment for the snake body.
+    :param x: int
+    :param y: int
+    :return: Turtle() object
+    """
+    turtle = Turtle(shape='square')
+    turtle.color('white')
+    turtle.penup()
+    turtle.goto(x, y)
+    return turtle
 
 
+def starting_position(a, b):
+    """
+    Creates the starting position for the snake starting with three segments.
+    :param a: int
+    :param b: int
+    :return: None
+    """
+    start_x = a
+    start_y = b
+
+    for i in range(3):
+        snake_body.append(create_segment(start_x, start_y))
+        start_x -= 20
 
 
+starting_position(x, y)
+print(snake_body)
 screen.exitonclick()
