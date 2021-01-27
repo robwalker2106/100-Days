b23968f64d7808c1e943017d73d007718e4fe857
robwalker2106 <blkdragon2106@gmail.com>
2021-01-26 20:53:05 -0500

    Class
    Create a snake class

--- Day-20-Snake-Game-Part-1/main.py
+++ Day-20-Snake-Game-Part-1/main.py
@@ -1,5 +1,5 @@
 from turtle import Screen
-from snake import create_segment, starting_position, move_snake
+from snake import Snake
 import time
 
 screen = Screen()
@@ -7,17 +7,41 @@ screen.setup(width=600, height=600)
 screen.bgcolor('black')
 screen.title('Snake Game')
 screen.tracer(0)
+screen.listen()
 
 # Starting coordinates for the snake.
-x, y = 0, 0
-snake_body = starting_position(x, y)
+#x, y = 0, 0
+snake_body = Snake()
 
-print(snake_body)
+snake_body.starting_position()
+
+
+# def turn_up():
+#     snake_body[0].setheading(90)
+#
+#
+# def turn_left():
+#     snake_body[0].setheading(180)
+#
+#
+# def turn_down():
+#     snake_body[0].setheading(270)
+#
+#
+# def turn_right():
+#     snake_body[0].setheading(0)
+
+
+screen.onkey(snake_body.turn_up, 'w')
+screen.onkey(snake_body.turn_left, 'a')
+screen.onkey(snake_body.turn_down, 's')
+screen.onkey(snake_body.turn_right, 'd')
 
 while 0 < 1:
-    move_snake(snake_body)
+    snake_body.move()
     screen.update()
     time.sleep(.1)
+
 
 
 screen.exitonclick()
--- Day-20-Snake-Game-Part-1/snake.py
+++ Day-20-Snake-Game-Part-1/snake.py
@@ -1,46 +1,88 @@
 from turtle import Turtle
 
 
-def create_segment(x, y):
-    """
-    Receives an x and y coordinate and creates a segment for the snake body.
-    :param x: int
-    :param y: int
-    :return: Turtle() object
-    """
-    turtle = Turtle(shape='square')
-    turtle.color('white')
-    turtle.penup()
-    turtle.goto(x, y)
-    return turtle
+class Snake:
+    def __init__(self):
+        self.x_start = 0
+        self.y_start = 0
+        self.snake_body = []
+
+    def starting_position(self):
+
+        for i in range(3):
+            self.snake_body.append(self.create_segment(self.x_start, self.y_start))
+            self.x_start -= 20
+
+    def create_segment(self, x, y):
+
+        turtle = Turtle(shape='square')
+        turtle.color('white')
+        turtle.penup()
+        turtle.goto(x, y)
+
+        return turtle
+
+    def move(self):
+        x, y = self.snake_body[0].position()
+
+        for i in range(len(self.snake_body)):
+            self.snake_body[-i].goto(x, y)
+
+        self.snake_body[0].forward(20)
+
+    def turn_up(self):
+        self.snake_body[0].setheading(90)
+
+    def turn_left(self):
+        self.snake_body[0].setheading(180)
+
+    def turn_down(self):
+        self.snake_body[0].setheading(270)
+
+    def turn_right(self):
+        self.snake_body[0].setheading(0)
+
+# def create_segment(x, y):
+#     """
+#     Receives an x and y coordinate and creates a segment for the snake body.
+#     :param x: int
+#     :param y: int
+#     :return: Turtle() object
+#     """
+#     turtle = Turtle(shape='square')
+#     turtle.color('white')
+#     turtle.penup()
+#     turtle.goto(x, y)
+#     return turtle
 
 
-def starting_position(a, b):
-    """
-    Creates the starting position for the snake starting with three segments.
-    :param a: int
-    :param b: int
-    :return: List of Turtle() objects.
-    """
-    start_x = a
-    start_y = b
-    snake_body = []
+# def starting_position(a, b):
+#     """
+#     Creates the starting position for the snake starting with three segments.
+#     :param a: int
+#     :param b: int
+#     :return: List of Turtle() objects.
+#     """
+#     start_x = a
+#     start_y = b
+#     snake_body = []
+#
+#     for i in range(3):
+#         snake_body.append(create_segment(start_x, start_y))
+#         start_x -= 20
 
-    for i in range(3):
-        snake_body.append(create_segment(start_x, start_y))
-        start_x -= 20
-
-    return snake_body
-
-
-def move_snake(snake_body):
-    """
-    Receives the list of Turtle() objects and move all of them by 5 paces.
-    :param snake_body: List of Turtle() objects
-    :return: None
-    """
-
-    for segment in snake_body:
-        segment.forward(5)
+    # return snake_body
 
 
+# def move_snake(snake_body):
+#     """
+#     Receives the list of Turtle() objects and move all of them by 20 paces.
+#     :param snake_body: List of Turtle() objects
+#     :return: None
+#     """
+#     x, y = snake_body[0].position()
+#
+#     for i in range(len(snake_body)):
+#         snake_body[-i].goto(x, y)
+#
+#     snake_body[0].forward(20)
