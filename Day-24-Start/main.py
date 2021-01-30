2fa908156c0df73fd26f5e969d421db6e31e3d5e
robwalker2106 <blkdragon2106@gmail.com>
2021-01-30 18:43:57 -0500

    update

--- Day-24-Start/main.py
+++ Day-24-Start/main.py
@@ -6,7 +6,7 @@ with open('./Input/Names/invited_names.txt') as file:
     data_line = file.readlines()
 
     for name in data_line:
-        names.append(name[:-1])
+        names.append(name.strip())
 
 with open('./Input/Letters/starting_letter.txt') as data:
     letter = data.read()
@@ -14,7 +14,7 @@ with open('./Input/Letters/starting_letter.txt') as data:
 for person in names:
     name = person
     new_letter = letter.replace('[name]', name)
-    with open('./Output/ReadyToSend/' + name + '.txt', 'w') as data:
+    with open('./Output/ReadyToSend/letter_for_' + name + '.txt', 'w') as data:
         data.write(new_letter)
 
 
