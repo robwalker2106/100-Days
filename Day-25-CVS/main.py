f1b8f9088b490e9400ca40c7438c71bf13166951
robwalker2106 <blkdragon2106@gmail.com>
2021-01-31 18:22:15 -0500

    update

--- Day-25-CVS/main.py
+++ Day-25-CVS/main.py
@@ -11,12 +11,27 @@ import pandas as pd
 #         if line[1] != 'temp':
 #             temperatures.append(int(line[1]))
 #     print(temperatures)
+#
+# weather_data = pd.read_csv('weather_data.csv')
+#
+# print(weather_data)
+#
+# weather_data.temp = weather_data.apply(lambda x: (int(x.temp) * 9/5) + 32, axis=1)
+#
+# print(weather_data)
 
-weather_data = pd.read_csv('weather_data.csv')
+squirrel_data = pd.read_csv('2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv')
 
-print(weather_data)
+# for color in new_colors:
+#     count.append(squirrel_data[squirrel_data['Primary Fur Color'] == color].count())
 
-weather_data.temp = weather_data.apply(lambda x: (int(x.temp) * 9/5) + 32, axis=1)
+gray_count = len(squirrel_data[squirrel_data['Primary Fur Color'] == 'Gray'])
+cinnamon_count = len(squirrel_data[squirrel_data['Primary Fur Color'] == 'Cinnamon'])
+black_count = len(squirrel_data[squirrel_data['Primary Fur Color'] == 'Black'])
 
-print(weather_data)
+new_data = {'Primary Fur Color': ['Gray', 'Cinnamon', 'Black'], 'Count': [gray_count, cinnamon_count, black_count]}
+
+new_pd = pd.DataFrame(new_data)
+
+new_pd.to_csv('squirrel_count.csv')
 
