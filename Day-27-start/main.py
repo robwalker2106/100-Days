a29c001638a1952f1bdbad76683637546be7a0b4
robwalker2106 <blkdragon2106@gmail.com>
2021-02-02 21:00:15 -0500

    Update main.py

--- Day-27-start/main.py
+++ Day-27-start/main.py
@@ -1,9 +1,32 @@
-def add(*args):
-    sum = 0
-    for x in args:
-        sum += x
+import tkinter
 
-    return sum
+window = tkinter.Tk()
+window.minsize(width=250, height=200)
+window.title("Miles to KM Converter")
+
+miles_input = tkinter.Entry(width=10)
+miles_input.grid(column=1, row=2)
+
+miles = tkinter.Label(text="Miles")
+miles.grid(column=2, row=2)
+
+is_equal_to = tkinter.Label(text="is equal to")
+is_equal_to.grid(column=0, row=3)
+
+miles_to_km = tkinter.Label(text="Press Calculate")
+miles_to_km.grid(column=1, row=3)
+
+Km = tkinter.Label(text="Km")
+Km.grid(column=2, row=3)
 
 
-print(add(3, 4, 5, 6))
+def calculate():
+    inputed_miles = int(miles_input.get())
+    km = (inputed_miles * 8) / 5
+    miles_to_km.config(text=km)
+
+
+calculate = tkinter.Button(text="Calculate", command=calculate)
+calculate.grid(column=1, row=4)
+
+window.mainloop()
