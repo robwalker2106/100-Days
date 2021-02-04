8b0673f2f705e0987ef57434edd3c567ce5ec5a6
robwalker2106 <blkdragon2106@gmail.com>
2021-02-03 20:31:31 -0500

    update

--- pomodoro-start/main.py
+++ pomodoro-start/main.py
@@ -1,3 +1,4 @@
+from tkinter import *
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -16,3 +17,29 @@ LONG_BREAK_MIN = 20
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 # ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+canvas = Canvas(width=204, height=224, bg=YELLOW, highlightthickness=0)
+tomato_img = PhotoImage(file="tomato.png")
+image = canvas.create_image(103, 112, image=tomato_img)
+canvas.create_text(103, 130, text="oo:oo", fill="white", font=(FONT_NAME, 35, 'bold'))
+canvas.grid(column=1, row=1)
+
+timer_text = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50, 'bold'))
+timer_text.grid(column=1, row=0)
+
+start_button = Button(text="Start", fg=GREEN, bg=YELLOW)
+start_button.grid(column=0, row=2)
+
+reset_button = Button(text="Reset", fg=GREEN, bg=YELLOW)
+reset_button.grid(column=2, row=2)
+
+check_mark = Label(text="✓", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 30, "bold"))
+check_mark.grid(column=1, row=3)
+
+
+
+window.mainloop()
