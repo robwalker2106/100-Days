641deca49f1b9624433df46b27ba0d953e05d059
robwalker2106 <blkdragon2106@gmail.com>
2021-02-04 21:19:42 -0500

    Update main.py

--- password-manager/main.py
+++ password-manager/main.py
@@ -1,5 +1,16 @@
+from tkinter import *
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.config(padx=20, pady=20)
+
+canvas = Canvas(width=200, height=200)
+lock_image = PhotoImage(file='logo.png')
+image = canvas.create_image(100, 100, image=lock_image)
+canvas.pack()
+
+window.mainloop()
