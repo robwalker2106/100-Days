5a66decb12e3a49eb204729b4c5fa311ed088551
robwalker2106 <blkdragon2106@gmail.com>
2021-02-09 14:13:45 -0500

    Update main.py

--- Day-32-Start/main.py
+++ Day-32-Start/main.py
@@ -1,10 +1,20 @@
 import smtplib
+import datetime as dt
+from random import choice
 
 my_email = "rob100python@yahoo.com"
 password = 'bocchmlhwyeprhsw'
 to_addrs = 'rob100python@gmail.com'
 
-with smtplib.SMTP("smtp.mail.yahoo.com") as connection:
-    connection.starttls()
-    connection.login(user=my_email, password=password)
-    connection.sendmail(from_addr=my_email, to_addrs=to_addrs, msg="Subject:Hello Test\n\nTesting")
+now = dt.datetime.now()
+
+if now.weekday() == 1:
+    with open('quotes.txt') as data:
+        lines = data.readlines()
+        quote = choice(lines)
+
+    with smtplib.SMTP("smtp.mail.yahoo.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(from_addr=my_email, to_addrs=to_addrs, msg=f"Subject: Today's Quote\n\n{quote}")
+    print('email sent')
