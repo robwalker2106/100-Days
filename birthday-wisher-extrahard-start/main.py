52fd122128ba1bf4d1b107eac2aed8239046050f
robwalker2106 <blkdragon2106@gmail.com>
2021-02-09 15:19:34 -0500

    complete

--- birthday-wisher-extrahard-start/main.py
+++ birthday-wisher-extrahard-start/main.py
@@ -1,11 +1,30 @@
-##################### Extra Hard Starting Project ######################
+import datetime as dt
+import smtplib
+from random import randint
+import pandas as pd
 
+my_email = "rob100python@yahoo.com"
+password = 'bocchmlhwyeprhsw'
 
-# 2. Check if today matches a birthday in the birthdays.csv
+birthdays_df = pd.read_csv('birthdays.csv')
+today = dt.datetime.now()
+todays_birthday = birthdays_df[(birthdays_df.month == today.month) & (birthdays_df.day == today.day)]
 
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
+for index, row in todays_birthday.iterrows():
+    letter = './letter_templates/letter_' + str(randint(1, 3)) + ".txt"
 
-# 4. Send the letter generated in step 3 to that person's email address.
+    with open(letter) as file:
+        letter_tmpl = file.read()
+        letter_tmpl = letter_tmpl.replace("[NAME]", row['name'])
+
+    with smtplib.SMTP('smtp.mail.yahoo.com') as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(from_addr=my_email,
+                            to_addrs=row['email'],
+                            msg=f'Subject: Happy Birthday\n\n{letter_tmpl}')
+    print(f'Email sent to {row["name"]}')
+
 
 
 
