290a72673c9e0e3bc9c3174b5613979bd02f8fd4
robwalker2106 <blkdragon2106@gmail.com>
2021-02-21 14:07:44 -0500

    Update main.py

--- quizzler-app-start/main.py
+++ quizzler-app-start/main.py
@@ -1,9 +1,14 @@
 from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
+import requests
 
+response = requests.get(url='https://opentdb.com/api.php?amount=10&type=boolean')
+response.raise_for_status()
+response_questions = response.json()
 question_bank = []
-for question in question_data:
+
+for question in response_questions['results']:
     question_text = question["question"]
     question_answer = question["correct_answer"]
     new_question = Question(question_text, question_answer)
