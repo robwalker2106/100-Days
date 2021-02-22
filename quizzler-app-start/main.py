6528ccc53655e8a43040ac35a5ad24a79150e010
robwalker2106 <blkdragon2106@gmail.com>
2021-02-22 10:38:37 -0500

    update

--- quizzler-app-start/main.py
+++ quizzler-app-start/main.py
@@ -1,6 +1,7 @@
 from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
+from ui import QuizInterface
 
 question_bank = []
 
@@ -13,8 +14,10 @@ for question in question_data:
 
 quiz = QuizBrain(question_bank)
 
-while quiz.still_has_questions():
-    quiz.next_question()
+# while quiz.still_has_questions():
+#     quiz.next_question()
+
+quiz = QuizInterface()
 
 print("You've completed the quiz")
 print(f"Your final score was: {quiz.score}/{quiz.question_number}")
