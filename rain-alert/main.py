41fef3ff059cfca924c8dc03d5639d4828a9c6ab
robwalker2106 <blkdragon2106@gmail.com>
2021-02-25 10:48:01 -0500

    Update main.py

--- rain-alert/main.py
+++ rain-alert/main.py
@@ -1,4 +1,8 @@
 import requests
+import os
+from twilio.rest import Client
+
+
 
 OWN_ENDPOINT = "https://api.openweathermap.org/data/2.5/onecall"
 api_key = "76b9233d1eb9b9e7aadfcdd0317c90b1"
@@ -20,6 +24,19 @@ umbrella = False
 for weather_id in twelve_hour:
     if weather_id['weather'][0]['id'] < 700:
         umbrella = True
+
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
+client = Client(account_sid, auth_token)
+
+message = client.messages \
+                .create(
+                     body="Join Earth's mightiest heroes. Like Kevin Bacon.",
+                     from_='+15017122661',
+                     to='+15558675310'
+                 )
+
+print(message.sid)
 
 print(umbrella)
 
