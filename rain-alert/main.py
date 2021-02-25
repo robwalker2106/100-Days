0ec7a689712677799db6bde2bf6a04ed90e6138c
robwalker2106 <blkdragon2106@gmail.com>
2021-02-24 19:33:43 -0500

    Update main.py

--- rain-alert/main.py
+++ rain-alert/main.py
@@ -5,7 +5,8 @@ api_key = "76b9233d1eb9b9e7aadfcdd0317c90b1"
 parameters = {'appid': api_key,
               'lat': 25.792240,
               'lon': -80.134850,
-              'units': 'imperial'}
+              'units': 'imperial',
+              'exclude': 'daily,minutely,current'}
 
 # API Request for the local weather
 response = requests.get(OWN_ENDPOINT, params=parameters)
@@ -13,7 +14,12 @@ response.raise_for_status()
 
 # Parsing the response from the website.
 weather = response.json()
-weather_hourly = weather['hourly']
+twelve_hour = weather['hourly'][:12]
+umbrella = False
 
-print(weather)
-print(weather_hourly)
+for weather_id in twelve_hour:
+    if weather_id['weather'][0]['id'] < 700:
+        umbrella = True
+
+print(umbrella)
+
